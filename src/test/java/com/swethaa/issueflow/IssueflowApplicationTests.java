6aba5029d0f039a651e062a9da21c19bdf9258b5
jvswethaa <jvswethaa@gmail.com>
2026-01-04 19:01:06 +0530

    Project setup with MySQL connection and /health endpoint

---
+++ src/test/java/com/swethaa/issueflow/IssueflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swethaa.issueflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
