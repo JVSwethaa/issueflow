f2559fbeb5ebe584c53b667e2b676ddf57ff7c8a
jvswethaa <jvswethaa@gmail.com>
2026-02-10 00:03:27 +0530

    Added custom JwtAuthFilter

---
+++ src/main/java/com/swethaa/issueflow/security/JwtAuthFilter.java
@@ -0,0 +1,55 @@
+package com.swethaa.issueflow.security;
+
+//import jakarta.servlet.http.HttpServlet;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+    private final JwtService jwtService;
+
+    public JwtAuthFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
+        String header = request.getHeader("Authorization");
+
+        if(header != null && header.startsWith("Bearer ")){
+            String token = header.substring(7);
+
+
+            if(jwtService.isTokenValid(token) && SecurityContextHolder.getContext().getAuthentication() == null){
+                String email = jwtService.extractEmail(token);
+                String role = jwtService.extractRole(token);
+
+                SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_"+role);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, null, List.of(authority));
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/auth") || path.equals("/health") || path.equals("/error");
+    }
+
+}
